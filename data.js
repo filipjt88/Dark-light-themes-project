c7b75ab2bd25f8c327002f9ef1b3352eda3f9ec8
filipjt88 <filip88ks@gmail.com>
2024-01-30 21:40:03 +0100

    save

---
+++ data.js
@@ -0,0 +1,25 @@
+const articles = [
+    {
+        id:1,
+        title: "Sports",
+        date: new Date(2023, 9, 14),
+        length: 11,
+        description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, nisi omnis quam ratione ea a veniam voluptates ullam dolore porro fuga est, architecto cupiditate tenetur dicta quidem minus velit debitis nesciunt aut nulla magnam culpa. Id sint ipsa, laboriosam neque, atque rerum adipisci amet repellendus facilis beatae esse, error labore.`
+    },
+    {
+        id:2,
+        title: "Cultures",
+        date: new Date(2023, 7, 22),
+        length: 5,
+        description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid placeat cupiditate provident possimus blanditiis recusandae. Natus voluptas sint aut quasi saepe aliquid aliquam, totam libero laborum? Officiis necessitatibus atque beatae provident quia, maiores ea id esse similique eius ab soluta tempore sint!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid placeat cupiditate provident possimus blanditiis recusandae. Natus voluptas sint aut quasi saepe aliquid aliquam, totam libero laborum? Officiis necessitatibus atque beatae provident quia, maiores ea id esse similique eius ab soluta tempore sint!`
+    },
+    {
+        id:1,
+        title: "Technology",
+        date: new Date(2024, 1, 30),
+        length: 3,
+        description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid placeat cupiditate provident possimus blanditiis recusandae. Natus voluptas sint aut quasi saepe aliquid aliquam, totam libero laborum? Officiis necessitatibus atque beatae provident quia, maiores ea id esse similique eius ab soluta tempore sint!`
+    }
+];
+
+
