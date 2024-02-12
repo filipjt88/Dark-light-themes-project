475f31ec77cad162a4248c3fb9c9ec86147f1910
filipjt88 <filip88ks@gmail.com>
2024-02-12 12:51:20 +0100

    save

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const changeThemes = document.querySelector(".btn");
 
 
+
 changeThemes.addEventListener("click", () => {
-    if(document.body.classList.toggle("light-theme") === "light-theme") {
-        document.body.classList.toggle("dark-themes");
-    } else if(document.body.classList.toggle("dark-themes") === 'dark-theme') {
-        document.body.classList.toggle("light-themes");
+    let dark = document.body.classList.toggle("dark-themes");
+    let light = document.body.classList.toggle("light-themes");
+    if(dark === "dark-themes") {
+        light = document.body.classList.toggle("light-themes");
+    } else if(light === 'light-themes') {
+        dark = document.body.classList.toggle("dark-themes");
+    } else {
+        light = document.body.classList.toggle("light-themes");
     }
 });
